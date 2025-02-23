32c4bd6cc6c50d6619b603fc06265055e53b80d9
Bakhytgali <omgwtfwat@gmail.com>
2025-02-23 22:03:37 +0500

    Get All Events #3

--- app/src/main/java/com/example/seeya/data/api/APIService.kt
+++ app/src/main/java/com/example/seeya/data/api/APIService.kt
@@ -9,6 +9,7 @@ import com.example.seeya.data.model.SignInRequest
 import com.example.seeya.data.model.SignInResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 
