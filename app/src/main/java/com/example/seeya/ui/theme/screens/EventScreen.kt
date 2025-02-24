53dfc88e1b5606d27b7b491cd57cd80eebacf356
Bakhytgali <omgwtfwat@gmail.com>
2025-02-24 06:21:00 +0500

    Joining the event

--- app/src/main/java/com/example/seeya/ui/theme/screens/EventScreen.kt
+++ app/src/main/java/com/example/seeya/ui/theme/screens/EventScreen.kt
@@ -1,6 +1,7 @@
 package com.example.seeya.ui.theme.screens
 
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,8 +10,11 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -22,9 +26,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.seeya.data.model.Event
 import com.example.seeya.ui.theme.Poppins
+import com.example.seeya.ui.theme.Unbounded
+import com.example.seeya.ui.theme.bgColor
 import com.example.seeya.ui.theme.components.EventTitleCard
 import com.example.seeya.ui.theme.components.MainScaffold
 import com.example.seeya.viewmodel.event.EventViewModel
@@ -82,6 +89,35 @@ fun EventScreen(
                         color = Color.Red,
                         fontFamily = Poppins,
                         fontWeight = FontWeight.Bold
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(30.dp))
+
+                Button(
+                    onClick = {
+                        event.value?.let {
+                            eventViewModel.joinEvent(
+                                eventId = it.eventId,
+                                onSuccess = {
+                                    Toast.makeText(context, "Successfully joined!", Toast.LENGTH_SHORT).show()
+                                },
+                                onError = { errorMsg ->
+                                    Toast.makeText(context, errorMsg, Toast.LENGTH_SHORT).show()
+                                }
+                            )
+                        }
+                    },
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Participate",
+                        color = bgColor,
+                        fontFamily = Unbounded,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        modifier = Modifier.padding(vertical = 10.dp)
                     )
                 }
 
